b12efe46430d1c8bdf77c52e30cdf84cd683a706
Rishab-Rawat <rawatrishab66@gmail.com>
2024-08-22 20:23:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,30 @@
 //complete this code
 class Animal {
-  constructor(species) {
-    this._species = species;
-  }
+    constructor(species) {
+        this._species = species;
+    }
 
-  get species() {
-    return this._species;
-  }
+    get species() {
+        return this._species;
+    }
 
-  makeSound() {
-    console.log(`The ${this.species} makes a sound`);
-  }
+    makeSound() {
+        console.log(The ${this._species} makes a sound);
+    }
 }
 
 class Cat extends Animal {
-  constructor(species) {
-    super(species);
-  }
-
-  purr() {
-    console.log("purr");
-  }
+    purr() {
+        console.log("purr");
+    }
 }
 
 class Dog extends Animal {
-  constructor(species) {
-    super(species);
-  }
-
-  bark() {
-    console.log("woof");
-  }
+    bark() {
+        console.log("woof");
+    }
 }
+
+window.Animal = Animal;
+window.Dog = Dog;
+window.Cat = Cat;
